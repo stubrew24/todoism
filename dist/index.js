46e9dbe6b3f725d398fe584b344aae1e215bd0df
Stuart Sewell <hello@stuartsewell.dev>
2020-02-11 16:52:49 +0000

    ts refactor

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Model_1 = require("./Model");
-var Controller_1 = require("./Controller");
-var View_1 = require("./View");
-var app = new Controller_1.default(new Model_1.default(), new View_1.default());
